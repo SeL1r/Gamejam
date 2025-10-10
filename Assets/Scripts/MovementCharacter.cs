a478c8e37d772cd3e48f2cd5390b43ed03720a67
SeL1r <froud2007@gmail.com>
2025-10-10 20:21:38 +0300

    Revert "Merge branch 'Roma'"
    This reverts commit edd0195a245186755c3abd6602c9d0173aa03acb, reversing
    changes made to 88e3fa557b1b245fea57f5179faf38d033616fd8.

--- Assets/Scripts/MovementCharacter.cs
+++ Assets/Scripts/MovementCharacter.cs
@@ -3,23 +3,15 @@ using UnityEngine.InputSystem;
 
 public class MovementCharacter : MonoBehaviour
 {
-	float maxHeadAngle;
-	float sensetivity;
-	public Camera mainCamera;
-	public GameObject head;
-	InputAction moveAction, jumpAction, sprintAction, lookAction;
+	InputAction moveAction, jumpAction, sprintAction;
 	Rigidbody rb;
-	float speedCharacter = 3, maxForceJump = 7;
+	float speedCharacter = 3, maxForceJump = 10;
 	void Start()
 	{
-		//Выгруженние сохранений
-		sensetivity = PlayerPrefs.GetFloat("Sensetivity");
-		
 		//Задаю переменный от сюда
 		moveAction = InputSystem.actions.FindAction("Move");
 		jumpAction = InputSystem.actions.FindAction("Jump");
 		sprintAction = InputSystem.actions.FindAction("Sprint");
-		lookAction = InputSystem.actions.FindAction("Look");
 		
 		rb = GetComponent<Rigidbody>();
 		//Задаю переменные до сюда
@@ -29,31 +21,12 @@ public class MovementCharacter : MonoBehaviour
 	{
 		//Передвижение игрока
 		Vector2 move = moveAction.ReadValue<Vector2>();
-		Vector3 localVelocity = new Vector3(move.x * speedCharacter, rb.linearVelocity.y, move.y * speedCharacter);
-		Vector3 globalVelocity = transform.TransformDirection(localVelocity);
-		rb.linearVelocity = globalVelocity;
+		Vector3 velocity = new Vector3(move.x * speedCharacter, rb.linearVelocity.y, move.y * speedCharacter);
+		rb.linearVelocity = velocity;
 	}
 	
 	void Update()
 	{
-		//Пускаем луч из центра
-		Vector3 centerPoint = new Vector3(Screen.width / 2, Screen.height / 2, 0);
-		Ray ray = mainCamera.ScreenPointToRay(centerPoint);
-		RaycastHit hit;
-		if(Physics.Raycast(ray, out hit))
-		{
-			GameObject hitObject = hit.collider.gameObject;
-		}
-
-		//Поворот камеры
-		Vector2 look = lookAction.ReadValue<Vector2>() * Time.deltaTime * sensetivity;
-		maxHeadAngle = Mathf.Clamp(maxHeadAngle - look.y, -70, 55);
-		if (look != Vector2.zero)
-		{
-			head.transform.localRotation = Quaternion.Euler(maxHeadAngle, 0, 0);
-			transform.Rotate(0, look.x, 0);
-		}
-		
 		//Прыжок Спринт
 		if(IsGrounde.isGrounded)
 		{
@@ -66,7 +39,7 @@ public class MovementCharacter : MonoBehaviour
 				speedCharacter = 3;
 			}
 			
-			if(jumpAction.IsPressed())
+			if(jumpAction.WasPressedThisFrame())
 			{
 				rb.AddForce(Vector3.up * maxForceJump, ForceMode.Impulse);
 			}
